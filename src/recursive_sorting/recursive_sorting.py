9a637bd7b2486ad1c766d629b5706fe5184a5881
Diana Ignatescu <diana.ignatescu8@gmail.com>
2019-06-04 14:03:07 +0100

    Implement quick_sort

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -1,10 +1,33 @@
-# TO-DO: complete the helpe function below to merge 2 sorted arrays
-def merge( arrA, arrB ):
-    elements = len( arrA ) + len( arrB )
+# TO-DO: complete the helper function below to merge 2 sorted arrays
+def merge(arrA, arrB):
+    elements = len(arrA) + len(arrB)
     merged_arr = [0] * elements
     # TO-DO
-    
+    a = 0
+    b = 0
+
+    for i in range(0, elements):
+        # if all elements in arrA have been merged, put next element in arrB into merged_arr
+        if len(arrA) <= a:
+            merged_arr[i] = arrB[b]
+            b += 1
+        # if all elements in arrB have been merged, put next element in arrA into merged_arr
+        elif len(arrB) <= b:
+            merged_arr[i] = arrA[a]
+            a += 1
+        # if next element in arrA is smaller, add to merged array
+        elif arrA[a] < arrB[b]:
+            merged_arr[i] = arrA[a]
+            a += 1
+        # if next element in arrB is smaller, add to merged array
+        else:
+            merged_arr[i] = arrB[b]
+            b += 1
+
     return merged_arr
+
+
+print(merge([4, 99, 13, 75, 2, 1, 178, 81], [3, 74, 11]))
 
 
 # TO-DO: implement the Merge Sort function below USING RECURSION
